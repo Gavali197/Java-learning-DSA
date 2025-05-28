856f4de17cbd3703daec30d187207894c0a4fb13
hemant gavali <digitaluser197@gmail.com>
2025-05-28 10:13:16 +0530

    Start Day 1

---
+++ DataType.java
@@ -0,0 +1,13 @@
+public class DataType {
+    public static void main(String[] args) {
+        int A = 849007757; //int can store 9 digit
+        long b = 6323620325523615165L; //java ko samaj me nhi aata ki ye long he isliye last me L lgate he 
+
+        String Nan = new String("hello bro"); //its non premetive data type this reason use new keyword
+        System.out.println(A);
+        System.out.println(Nan);
+        System.out.println(b);
+
+        // byte, short, int, long, float, double, char, boolean; this is all premetive data type
+    }
+}
---
+++ StringArray.java
@@ -0,0 +1,12 @@
+public class StringArray {
+    public static void main(String[] args) {
+        String Name = new String("Hemant is good devloper");
+        String About = new  String(" It is good knowledge of java");
+        String Hemant = Name + About;
+        System.out.println(Hemant); // this is concate process join two string 
+        System.out.println(Hemant.length()); //lenth 
+        System.out.println(Hemant.charAt(5)); // find char using index for charAt
+        System.out.println(Hemant.replace('t','n')); //replaceing process
+        System.out.println(Hemant.substring(16,32)); //use for subSTring
+    }
+}
