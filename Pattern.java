5c57c3570ea031e9b48225f9b0612f023dd7512b
hemant gavali <digitaluser197@gmail.com>
2025-06-23 01:30:08 +0530

    inverted Pattern ap-c

--- Pattern.java
+++ Pattern.java
@@ -36,16 +36,41 @@ public class Pattern {
 
 
 
-        for(int i=1; i<=5; i++){
-            // print for loop are spce 
-            for(int j=1; j<=5-i; j++){
+        // for(int i=1; i<=5; i++){
+        //     // print for loop are spce 
+        //     for(int j=1; j<=5-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j<= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+        // int n = 4;
+        // int m = 5;
+
+        // for(int i=n; i>=1; i--){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // inverted half piramint (rotated by 180 deg)
+        
+        int n=4;
+        for(int i = 1; i<= n; i++){
+            for(int j = 1; j <= n-i; j++){
                 System.out.print(" ");
             }
-            for(int j=1; j<= i; j++){
+
+            for(int j = 1; j<=i; j++){
                 System.out.print("*");
             }
-            System.out.println(" ");
+            System.out.println();
         }
+    
 
     }
 }
