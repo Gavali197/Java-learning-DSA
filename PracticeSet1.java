4e71db6288ecbfff85d5004d8456daef9b6b8701
hemant gavali <digitaluser197@gmail.com>
2025-06-24 23:46:22 +0530

    practice set 1

---
+++ PracticeSet1.java
@@ -0,0 +1,50 @@
+public class PracticeSet1 {
+    public static void main(String[] args) {
+        /*  que 1
+         *      *****
+         *      *****
+         *      *****
+         *      *****
+         */
+
+        //  int n = 4;
+        //  int m = 5;
+
+        //  for(int i = 1; i<= n; i++){
+        //     for(int j = 1; j<=m; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        //  }
+
+
+
+        // que 2
+        /*
+         *          *****
+         *          *   *
+         *          *   *
+         *          *****
+         */
+
+
+        //  int n = 5;
+        //  int m = 4;
+
+        //  for(int i = 0; i< n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         if(i == 0 || i == n-1 || j == 0 || j == m-1){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        
+
+
+    }
+}
