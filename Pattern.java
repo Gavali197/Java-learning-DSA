cc27376b1fb5da3e1288ea4e951383cbe237ca55
hemant gavali <digitaluser197@gmail.com>
2025-06-24 23:21:00 +0530

    Start Advance Pattern

--- Pattern.java
+++ Pattern.java
@@ -92,8 +92,30 @@ public class Pattern {
         //     }
         //     System.out.println(" ");
         // }
-    
         
+        // floid piramint pattern 
+
+        int n = 5;
+        int number = 1;
+
+        for(int i = 1; i <= n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
 
     }
 }
+
+
+
+
+
+
+    //     1
+    //    121
+    //   12321
+    //  1234321
+    // 123454321
