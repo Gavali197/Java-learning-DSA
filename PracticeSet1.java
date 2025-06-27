2bf461a53a291dee07978b865137507a17103be3
hemant gavali <digitaluser197@gmail.com>
2025-06-27 11:22:32 +0530

    practiceSet2

--- PracticeSet1.java
+++ PracticeSet1.java
@@ -43,8 +43,13 @@ public class PracticeSet1 {
         // }
 
 
-        
 
+        for(int i = 1; i <= 5; i++){
+            for(int j =1; j<= i; j++){
+                System.out.print(j + " " );
+            }
+            System.out.println("");
+        }
 
     }
 }
