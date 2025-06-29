1877c0069185a1356b0794ad3aecffb7c0173095
hemant gavali <digitaluser197@gmail.com>
2025-06-29 11:01:34 +0530

    starting Arrays

---
+++ Practice/ArrayPractice1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+
+public class ArrayPractice1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 1 take an array of names as input from the user and print them on the screen.
+        int number = 0;
+        
+
+      
+    }
+}
