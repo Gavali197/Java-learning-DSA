4f0b59cce6152e0d386095931ba33ea404a04939
hemant gavali <digitaluser197@gmail.com>
2025-06-29 11:15:31 +0530

    MinMax Arrays

--- Practice/ArrayPractice1.java
+++ Practice/ArrayPractice1.java
@@ -9,9 +9,15 @@ public class ArrayPractice1 {
         // 1 take an array of names as input from the user and print them on the screen.
         int array[] = {1, 2,3, 4,5};
         int sum = 0;
-        for (int num : array) {
-            sum = num + sum;
-        }
-        System.out.println(sum);
+        // for (int num : array) {
+        //     sum = num + sum;
+        // }
+        // System.out.println(sum);
+
+        /**Find Maximum and Minimum
+        Write a Java program to find the largest and smallest element in an array.**/
+
+        
+
     }
 }
