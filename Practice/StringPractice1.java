253adb125c27059dfdd32a411966d63dc16f1492
hemant gavali <digitaluser197@gmail.com>
2025-07-03 23:33:03 +0530

    start today dsa frist

--- Practice/StringPractice1.java
+++ Practice/StringPractice1.java
@@ -1,20 +1,41 @@
 
 import java.util.Scanner;
 
+
+
 public class StringPractice1 {
     public static void main(String[] args) {
         // 1 input user array string calculate string length
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" Enter String   :  ");
-        int size = sc.nextInt();
-        String arrays[] = new String[size];
-        int tot = 0;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println(" Enter String   :  ");
+        // int size = sc.nextInt();
+        // String arrays[] = new String[size];
+        // int tot = 0;
 
-        for(int i = 0; i < size; i++){
-            arrays[i] = sc.next();
-            tot += arrays[i].length();
-        }
-        System.out.println(tot);
+        // for(int i = 0; i < size; i++){
+        //     arrays[i] = sc.next();
+        //     tot += arrays[i].length();
+        // }
+        // System.out.println(tot);
+
+
+
+        // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+        // helloName("Bob") → "Hello Bob!"
+        // helloName("Alice") → "Hello Alice!"
+        // helloName("X") → "Hello X!
+
+        Scanner sc = new Scanner(System.in);
+
+        // String Hello = "Hello!";
+        // System.out.println("enter your name : ");
+        // String Name = sc.next();
+
+        // System.out.println(Hello+ " "+ Name);
+      
+
     }
 }
