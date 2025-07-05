192174d54bdc28293e2a2bf6655c9ff3d8ef9fc0
hemant gavali <digitaluser197@gmail.com>
2025-07-05 11:41:40 +0530

    practice Bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -20,7 +20,26 @@ public class BubbleSort {
     }
 
 
+    static void BubbleSort1(){
+        int arr[] = {16, 15, 9, 1, 2, 5};
+
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length -i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(" " + arr[i]);
+        }
+    }
+
+
     public static void main(String[] args) {
+        BubbleSort1();
        
         /*  int arr[] = {7, 8, 3, 9, 1, 2};
 
