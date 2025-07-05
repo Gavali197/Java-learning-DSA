c6d47872e92d32700497cdbe63674ba1e2e91ef9
hemant gavali <digitaluser197@gmail.com>
2025-07-05 11:50:30 +0530

    practice selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,25 @@
 
 public class SelectionSort {
 
+    static void SelectionSortArr(){
+        int arr[] = {16,15, 1, 2 , 8, 10, 9};
+
+        for(int i = 0; i < arr.length; i++){
+            int small = i;
+            for(int j = i +1; j < arr.length; j++){
+                if(arr[small] > arr[j]){
+                    small = j;
+                }
+            }
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i =0; i < arr.length; i++){
+            System.out.print(" " + arr[i]);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {5, 6, 1, 2, 4, 3};
 
@@ -19,5 +38,7 @@ public class SelectionSort {
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+
+        SelectionSortArr();
     }
 }
