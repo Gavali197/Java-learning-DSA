f76fe0577a5dbc2a7a529707004c440cd58c061f
hemant gavali <digitaluser197@gmail.com>
2025-07-11 18:18:41 +0530

    reverce string using recrusion

---
+++ Recrusion/level2/StrReverse.java
@@ -0,0 +1,17 @@
+package Recrusion.level2;
+
+public class StrReverse {
+    public static void revString(String str, int idx){
+       if(idx == 0){
+        System.out.println(str.charAt(idx));
+        return;
+       }
+       System.out.println(str.charAt(idx));
+       revString(str, idx-1);
+
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        revString(str, str.length()-1);
+    }
+}
