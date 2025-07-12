5b07a0e0d37fa3fea4b118b09f6f2d00cd3c8ebe
hemant gavali <digitaluser197@gmail.com>
2025-07-12 11:40:27 +0530

    Frist and Last Occurance

---
+++ Recrusion/level2/FristLastOccurance.java
@@ -0,0 +1,27 @@
+package Recrusion.level2;
+
+public class FristLastOccurance {
+    public static int Frist = -1;
+    public static int Last = -1;
+    public static void fristOccurance(String str, int idx, char element){
+        if(idx == str.length()){
+            System.out.println(Frist);
+            System.out.println(Last);
+            return;
+        }
+        char curr = str.charAt(idx);
+        if(curr == element){
+            if(Frist == -1){
+                Frist = idx;
+            }else{
+                Last = idx;
+            }
+        }
+        fristOccurance(str, idx+1, element);
+    }
+
+    public static void main(String[] args) {
+        String str = "abaacdaefaah";
+        fristOccurance(str, 0, 'a');
+    }
+}
