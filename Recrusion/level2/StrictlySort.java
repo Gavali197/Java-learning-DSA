5ed9001d3c9d9a4ce27a1bb463a57aae5e4c0e89
hemant gavali <digitaluser197@gmail.com>
2025-07-13 01:24:08 +0530

    isSorted

---
+++ Recrusion/level2/StrictlySort.java
@@ -0,0 +1,20 @@
+package Recrusion.level2;
+
+public class StrictlySort {
+    public static boolean isSorted(int arr[], int idx){
+        if(idx == arr.length-1){
+            return true;
+        }
+        
+        if(arr[idx] < arr[idx+1]){
+            return isSorted(arr, idx+1);
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,5};
+        System.out.println(isSorted(arr, 0));
+    }
+}
