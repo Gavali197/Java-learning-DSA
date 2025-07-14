84644f5394cce722cb6de15cbe06c7a1c50eeb2e
hemant gavali <digitaluser197@gmail.com>
2025-07-14 23:42:02 +0530

    Move all X end of the side

---
+++ Recrusion/level2/MoveEndOfX.java
@@ -0,0 +1,25 @@
+package Recrusion.level2;
+
+public class MoveEndOfX {
+    public static void xMove(String newString, String str, int idx, int count){
+        if(idx == str.length()){
+            for(int i = 0; i < count; i++){
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == 'x'){
+            count++;
+            xMove(newString, str, idx+1, count);
+        }else{
+            newString+=currChar;
+            xMove(newString, str, idx+1, count);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abxxcdxxexfjiax"; 
+        xMove("", str, 0, 0);
+    }
+}
