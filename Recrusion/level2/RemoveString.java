6fd8364354a41c98a69c96d94abe3c122406096b
hemant gavali <digitaluser197@gmail.com>
2025-07-18 00:01:37 +0530

    remove String

---
+++ Recrusion/level2/RemoveString.java
@@ -0,0 +1,24 @@
+package Recrusion.level2;
+
+public class RemoveString {
+    public static boolean map[] = new boolean[26];
+
+    public static void removeDublicate(String str, int idx, String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a']){
+            removeDublicate(str, idx+1, newString);
+        }else{
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDublicate(str, idx+1, newString);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abbccda";
+        removeDublicate(str, 0, "");
+    }
+}
