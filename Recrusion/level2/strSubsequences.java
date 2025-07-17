c98209e177cb03c4bb7008f1be6e6d3326cab312
hemant gavali <digitaluser197@gmail.com>
2025-07-18 00:58:10 +0530

    mobile key pad matching combination quetion quetion

--- Recrusion/level2/strSubsequences.java
+++ Recrusion/level2/strSubsequences.java
@@ -2,6 +2,24 @@ package Recrusion.level2;
 
 public class strSubsequences {
 
+    //mobile key pad matching combination quetion quetion 
+    public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+    public static void printCombo(String newString, String str, int idx) {
+        if (idx == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        String mappinng = keypad[currChar - '0'];
+
+        for (int i = 0; i < mappinng.length(); i++) {
+            printCombo(newString + mappinng.charAt(i), str, idx + 1);
+        }
+    }
+
+    //subsequence of all char
     public static void Subsequence(String str, String newString, int idx) {
         if (idx == str.length()) {
             System.out.println(newString);
@@ -17,8 +35,8 @@ public class strSubsequences {
 
     public static void main(String[] args) {
 
-        String str = "abc";
-
-        Subsequence(str, "", 0);
+        String str = "23";
+        printCombo("", str, 0);
+        // Subsequence(str, "", 0);
     }
 }
