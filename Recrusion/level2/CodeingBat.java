0755a99284b7b964d4431b5ee2e393bf42d47a13
hemant gavali <digitaluser197@gmail.com>
2025-07-19 12:02:19 +0530

    count x

--- Recrusion/level2/CodeingBat.java
+++ Recrusion/level2/CodeingBat.java
@@ -1,7 +1,32 @@
 package Recrusion.level2;
 
 public class CodeingBat {
+
+// QUETION 1 COUNT X 
+
+
+// Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+
+
+// countX("xxhixx") → 4
+// countX("xhixhix") → 3
+// countX("hi") → 0
+
+    //sovle my hand other logic 
+    public static void xCount(int idx, int count, String str){
+        if(idx == str.length()){
+            System.out.println(count);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == 'x'){
+            count++;
+        }
+        xCount(idx+1, count, str);
+    }
+
     public static void main(String[] args) {
-        
+        String str = "xxxabcxx";
+        xCount(0, 0, str);
     }
 } 
