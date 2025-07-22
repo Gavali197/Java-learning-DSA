d6d3a4b40448d46faf2f3437638e4d25ca3edc62
hemant gavali <digitaluser197@gmail.com>
2025-07-22 17:29:00 +0530

    count tiles way class 3

--- Recrusion/level2/CodeingBat.java
+++ Recrusion/level2/CodeingBat.java
@@ -141,8 +141,6 @@ public class CodeingBat {
 // array11([1, 2, 11], 0) → 1
 // array11([11, 11], 0) → 2
 // array11([1, 2, 3, 4], 0) → 0
-
-
     public int array11(int[] nums, int index) {
         if (index >= nums.length) {
             return 0;
@@ -155,17 +153,17 @@ public class CodeingBat {
     }
 
     // SAME QUETION SOVLE WITH OUT COUNT VERIABLE 
-
     public int array11Count(int[] nums, int index) {
-        if(nums[index] == 11){
-            return 1 + array11(nums, index+1);
-        }else{
-            return array11(nums, index+1);
+        if (index >= nums.length) {
+            return 0;
+        }
+
+        if (nums[index] == 11) {
+            return 1 + array11(nums, index + 1);
+        } else {
+            return array11(nums, index + 1);
         }
     }
-
-
-
 
     public static void main(String[] args) {
 
---
+++ Recrusion/level3/CountTilesWay.java
@@ -0,0 +1,22 @@
+package Recrusion.level3;
+
+public class CountTilesWay {
+    public static int countWay(int n, int m){
+        //base case 
+        if(n==m){
+            return 2;
+        }
+        if(n < m){
+            return 1;
+        }
+        //vertically place tiles
+        int vertically = countWay(n-m, m);
+        //horizontally place tiles 
+        int horizontally = countWay(n-1, m);
+
+        return vertically + horizontally;
+    }
+    public static void main(String[] args) {
+        System.out.println(countWay(12, 8));
+    }
+}
