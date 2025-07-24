d5a86138763d229f24fe421853dce685d1aceaeb
hemant gavali <digitaluser197@gmail.com>
2025-07-24 11:44:50 +0530

    all star

--- Recrusion/level2/CodeingBat.java
+++ Recrusion/level2/CodeingBat.java
@@ -190,7 +190,21 @@ public class CodeingBat {
 
 
 
+//QUETION 8 ALLSTAR
+// Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
 
+
+// allStar("hello") → "h*e*l*l*o"
+// allStar("abc") → "a*b*c"
+// allStar("ab") → "a*b"
+
+
+    public String allStar(String str) {
+  if(str.length() <= 1){
+    return str;
+  }
+  return str.charAt(0) + "*" + allStar(str.substring(1));
+}
 
 
     public static void main(String[] args) {
