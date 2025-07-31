7ac3fe8edd85fa850e9ca6588206659f0f8f4212
hemant gavali <digitaluser197@gmail.com>
2025-07-31 23:32:35 +0530

    count even number

--- Practice/ArrayPractice1.java
+++ Practice/ArrayPractice1.java
@@ -69,6 +69,24 @@ public class ArrayPractice1 {
         }
     }
 
+    // QUETION COUNTEVEN IN ARRAY
+
+    // Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+    // countEvens([2, 1, 2, 3, 4]) → 3
+    // countEvens([2, 2, 0]) → 3
+    // countEvens([1, 3, 5]) → 0
+    
+    public int countEvens(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
