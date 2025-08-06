53293942ba838b89356fb70bfa4d0d5b5449299e
hemant gavali <digitaluser197@gmail.com>
2025-08-06 23:33:41 +0530

    possible grup in array

--- Recrusion/level3/codeingBat.java
+++ Recrusion/level3/codeingBat.java
@@ -27,6 +27,7 @@ public class codeingBat {
         return false;
     }
 
+    //quetion 2 
     // Given an array of ints, is it possible to choose a group of some of the ints, beginning at the start index, 
     // such that the group sums to the given target?
     // However, with the additional constraint that all 6's must be chosen. (No loops needed.)
