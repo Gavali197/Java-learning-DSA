97c3baf10c7e9cfa49be1ffaaf9995e42a0e4bdc
hemant gavali <digitaluser197@gmail.com>
2025-08-06 23:32:40 +0530

    array ints

--- Recrusion/level3/codeingBat.java
+++ Recrusion/level3/codeingBat.java
@@ -1,8 +1,9 @@
 package Recrusion.level3;
 
 public class codeingBat {
-
-// Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target?
+    
+    //quetion 1
+ // Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target?
     //This is a classic backtracking recursion problem. Once you understand the recursive backtracking strategy in this problem, you 
 // can use the same pattern for many problems to search a space of choices. Rather than looking at the whole array, our convention 
     //is to consider the part of the array starting at index start and continuing to the end of the array. The caller can specify the 
