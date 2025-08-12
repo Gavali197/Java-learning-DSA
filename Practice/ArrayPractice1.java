a8b00567905b6b078ebbc1bd00f1698f4d496b3a
hemant gavali <digitaluser197@gmail.com>
2025-08-12 12:09:54 +0530

    array practice

--- Practice/ArrayPractice1.java
+++ Practice/ArrayPractice1.java
@@ -70,12 +70,10 @@ public class ArrayPractice1 {
     }
 
     // QUETION COUNTEVEN IN ARRAY
-
     // Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
     // countEvens([2, 1, 2, 3, 4]) → 3
     // countEvens([2, 2, 0]) → 3
     // countEvens([1, 3, 5]) → 0
-    
     public int countEvens(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -87,17 +85,22 @@ public class ArrayPractice1 {
         return count;
     }
 
-    
 // Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
-
-
 // sum13([1, 2, 2, 1]) → 6
 // sum13([1, 1]) → 2
 // sum13([1, 2, 2, 1, 13]) → 6
+    public int sum13(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 13) {
+                i++;
+            } else {
+                sum += nums[i];
+            }
 
-
-
-
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
 
