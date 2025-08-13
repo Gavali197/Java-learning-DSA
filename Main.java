4d3903e44ed7aac3d1f0a2e85d47615b27940a1f
hemant gavali <digitaluser197@gmail.com>
2025-08-13 18:21:59 +0530

    print linklist

--- Main.java
+++ Main.java
@@ -31,29 +31,56 @@ import java.util.*;
 
 // }
 
-class Node{
+// Node class (single responsibility: store data)
+class Node {
     int data;
-    int next;
+    Node next;
 
-    Node(int data){
+    Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
 
-class LinkList{
-   Node head;
+// LinkedList class (manages nodes)
+class LinkedList {
+    Node head;
 
-   public void add(int data){
-    Node newNode = new Node(data);
+    // Add node at end
+    public void add(int data) {
+        Node newNode = new Node(data);
 
-    if(head == null){
-        head = newNode;
-    }else{
-        Node curr = head;
-        while (curr.next != null) { 
-            curr = curr.next;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
         }
     }
-   }
+
+    // Print all nodes
+    public void printList() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+}
+
+// Main class (runs the program)
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(1);
+        list.add(5);
+        list.add(7);
+
+        list.printList(); // Output: 1 -> 5 -> 7 -> null
+    }
 }
