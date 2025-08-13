ec9df7fc3b6ecfdb66f293c14bed817529187002
hemant gavali <digitaluser197@gmail.com>
2025-08-13 18:06:54 +0530

    rouff work

--- RouffWork.java
+++ RouffWork.java
@@ -1,27 +1,30 @@
 
 public class RouffWork {
 
-    public static void main(String[] args) {
-        for (int i = 4; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
+    // public static void main(String[] args) {
+    //     for (int i = 4; i >= 1; i--) {
+    //         for (int j = 1; j <= i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println(" ");
 
-        }
-    }
+    //     }
+    // }
 
 
-    public String longestCommonPrefix(String[] strs) {
-        for(int i = 0; i < strs[0].length(); i++){
-            char currChar = strs[0].charAt(i);
+    // public String longestCommonPrefix(String[] strs) {
+    //     for(int i = 0; i < strs[0].length(); i++){
+    //         char currChar = strs[0].charAt(i);
 
-            for(int j = 1; j < strs.length; j++){
-                if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
-                    return strs[0].substring(0, i);
-                }
-            }
-        }
-        return strs[0];
-    }
+    //         for(int j = 1; j < strs.length; j++){
+    //             if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+    //                 return strs[0].substring(0, i);
+    //             }
+    //         }
+    //     }
+    //     return strs[0];
+    // }
+
+
+    
 }
