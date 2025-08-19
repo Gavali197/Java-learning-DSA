2e705d8d60227f60e963f3480f90bab5b69a5e8e
hemant gavali <digitaluser197@gmail.com>
2025-08-19 17:56:12 +0530

    main java

--- Main.java
+++ Main.java
@@ -1,34 +1,3 @@
-
-//     // public static void main(String[] args) {
-//     //     for (int i = 4; i >= 1; i--) {
-//     //         for (int j = 1; j <= i; j++) {
-//     //             System.out.print("*");
-//     //         }
-//     //         System.out.println(" ");
-
-//     //     }
-//     // }
-
-
-//     // public String longestCommonPrefix(String[] strs) {
-//     //     for(int i = 0; i < strs[0].length(); i++){
-//     //         char currChar = strs[0].charAt(i);
-
-//     //         for(int j = 1; j < strs.length; j++){
-//     //             if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
-//     //                 return strs[0].substring(0, i);
-//     //             }
-//     //         }
-//     //     }
-//     //     return strs[0];
-//     // }
-
-
-    
-
-// }
-
-// Node class (single responsibility: store data)
 class Node {
     int data;
     Node next;
