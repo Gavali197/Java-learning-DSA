e447e86a8960916a4f0b1720c23e646d72bac519
hemant gavali <digitaluser197@gmail.com>
2025-08-19 17:57:49 +0530

    manage

--- Backtracking.java
+++ Backtracking.java
@@ -1,1 +1,18 @@
-fd
+public class Backtracking {
+    //analysis backtracking
+    public static void backtrack(String str, String permu, int idx){
+        if(str.length() == 0){
+            System.out.println(permu);
+            return;
+        }
+
+        for(int i =0; i < str.length(); i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            backtrack(newStr, permu+ currChar, idx +1);
+        }
+    }
+    public static void main(String[] args) {
+        backtrack("abc", "", 0);
+    }
+}
