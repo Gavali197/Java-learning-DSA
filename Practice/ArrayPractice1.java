a0fea730ddbab2513d249d724b2f34ff51e97ba0
hemant gavali <digitaluser197@gmail.com>
2025-08-22 11:56:16 +0530

    count even

--- Practice/ArrayPractice1.java
+++ Practice/ArrayPractice1.java
@@ -69,21 +69,7 @@ public class ArrayPractice1 {
         }
     }
 
-    // QUETION COUNTEVEN IN ARRAY
-    // Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
-    // countEvens([2, 1, 2, 3, 4]) → 3
-    // countEvens([2, 2, 0]) → 3
-    // countEvens([1, 3, 5]) → 0
-    public int countEvens(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                count++;
-            }
-        }
 
-        return count;
-    }
 
 // Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
 // sum13([1, 2, 2, 1]) → 6
