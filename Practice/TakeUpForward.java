0f87190e7876d84c5fb5281be8590165f698f561
hemant gavali <digitaluser197@gmail.com>
2025-08-23 11:59:31 +0530

    reverse int using math

--- Practice/TakeUpForward.java
+++ Practice/TakeUpForward.java
@@ -1,13 +1,6 @@
 public class TakeUpForward {
     public static void main(String[] args) {
-        int n = 12345;   // Example input
-        int count = 0;
-
-        while (n > 0) {
-            n = n / 10;   // remove last digit
-            count++;      // increase digit count
-        }
-
-        System.out.println(count);  // Output: 5
+     int n = 13456;
+     
     }
 }
