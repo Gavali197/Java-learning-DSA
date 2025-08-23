a4630e97c819d4b32e8afa01cf9ffcd3c32e2eab
hemant gavali <digitaluser197@gmail.com>
2025-08-23 12:01:17 +0530

    sovle

--- Practice/TakeUpForward.java
+++ Practice/TakeUpForward.java
@@ -1,6 +1,16 @@
 public class TakeUpForward {
+
+    public int reverseNumber(int number) {
+    int reversed = 0;
+    while (number != 0) {
+        int digit = number % 10; // Get the last digit
+        reversed = reversed * 10 + digit; // Append the digit to the reversed number
+        number /= 10; // Remove the last digit from the original number
+    }
+    return reversed;
+}
+
     public static void main(String[] args) {
-     int n = 13456;
-     
+        
     }
 }
