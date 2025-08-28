639446d7711baacdb3c8df9c5081cd9114935715
hemant gavali <digitaluser197@gmail.com>
2025-08-28 12:01:01 +0530

    count even node in linkelist

---
+++ Practice/Linkelist/CountEven.java
@@ -0,0 +1,21 @@
+package Linkelist;
+
+class node{
+    int data;
+    node next;
+
+    node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class linked{
+    
+}
+
+public class CountEven {
+    public static void main(String[] args) {
+        
+    }
+}
