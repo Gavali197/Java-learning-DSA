e60f87060221f683af9d52d025c6eced1328dfea
hemant gavali <digitaluser197@gmail.com>
2025-08-29 11:47:13 +0530

    intial commit

--- Practice/Linkelist/LevelA.java
+++ Practice/Linkelist/LevelA.java
@@ -11,13 +11,35 @@ class node{
 }
 
 class linked{
+    node head;
     public void addlast(int data){
+        node newNode = new node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
 
+        node currNode = head;
+
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void print(){
+        node currNode = head;
+
+        while (currNode != null) {
+            System.out.println(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
     }
 }
 
 public class LevelA {
     public static void main(String[] args) {
-        
+     System.out.println("null");   
     }
 }
