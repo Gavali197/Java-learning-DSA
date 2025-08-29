a00b3b9b9e7e79109f21309cd14b071e22d69713
hemant gavali <digitaluser197@gmail.com>
2025-08-29 11:38:37 +0530

    inserting node by node

---
+++ Practice/Linkelist/LevelA.java
@@ -0,0 +1,23 @@
+package Linkelist;
+
+class node{
+    int data;
+    node next;
+
+        node(int data){
+            this.data = data;
+            this.next = null;
+        }
+}
+
+class linked{
+    public void addlast(int data){
+
+    }
+}
+
+public class LevelA {
+    public static void main(String[] args) {
+        
+    }
+}
